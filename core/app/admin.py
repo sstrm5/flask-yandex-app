383b4fe1666041d234538be1355113c6be978221
sstrm5 <hatedyoufromhello@yandex.ru>
2025-05-14 12:23:10 +0400

    ограничил доступ к админке

--- core/app/admin.py
+++ core/app/admin.py
@@ -1,4 +1,4 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for
@@ -8,16 +8,32 @@ from core.app.users.models import User
 
 
 # Ограничение доступа в админке
-class AdminOnlyView(ModelView):
+class MyModelView(ModelView):
     def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin
+        return current_user.is_authenticated and current_user.role.value == "admin"
+
+    def can_view(self):
+        return current_user.is_authenticated and current_user.role.value == "admin"
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for("login"))
+        return redirect(url_for("api.users.login"))
+
+
+class AdminOnlyView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.role.value == "admin"
+
+    def can_view(self):
+        return current_user.is_authenticated and current_user.role.value == "admin"
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("api.users.login"))
 
 
 # Функция для инициализации админки
 def init_admin(app):
-    admin = Admin(app, name="Админка", template_mode="bootstrap4")
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(News, db.session))
+    admin = Admin(
+        app, name="Админка", template_mode="bootstrap4", index_view=AdminOnlyView()
+    )
+    admin.add_view(MyModelView(User, db.session))
+    admin.add_view(MyModelView(News, db.session))
